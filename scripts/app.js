2173e252db423db67a67cd64259b8a5ff1ed51cd
andela-akangethe <alex.kangethe@andela.com>
2016-05-25 11:12:39 +0300

    Added data

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,12 @@
-(function () {
+(function() {
   'use strict';
 
-  angular.module('ngClassifieds', []);
+  angular
+    .module('ngClassifieds', ['ngMaterial'])
+    .config(function($mdThemingProvider) {
+      $mdThemingProvider.theme('default')
+        .primaryPalette('teal')
+        .accentPalette('orange');
+    });
 
 })();
