3037912c6c2b5294dbc45dcde7709c227a6f6c2b
andela-akangethe <alex.kangethe@andela.com>
2016-05-25 16:28:17 +0300

    Intro to uirouters

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,21 @@
   'use strict';
 
   angular
-    .module('ngClassifieds', ['ngMaterial'])
-    .config(function($mdThemingProvider) {
+    .module('ngClassifieds', ['ngMaterial', 'ui.router'])
+    .config(function($mdThemingProvider, $stateProvider) {
       $mdThemingProvider.theme('default')
         .primaryPalette('teal')
         .accentPalette('orange');
+
+      $stateProvider
+        .state('state', {
+          url: '/stateone',
+          template: '<h1>State One </h1>'
+        })
+        .state('statetwo', {
+          url: '/statetwo',
+          template: '<h1>State Two  </h1>'
+        })
     });
 
 })();
